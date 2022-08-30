ccf999a55ce1c458abed8710368fa1741fce25ec
smohebbi <siavash.mohebbi@gmail.com>
2022-08-30 15:35:46 +0200

    update redirect

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -22,10 +22,11 @@ export const Spotify = {
         }
     },
 
+
     search(term) {
         const accessToken = this.getAccessToken();
         return fetch(
-            `https://api.spotify.com/v1/search?type=track&q=${term}&`,
+            `https://api.spotify.com/v1/search?type=track&q=${term}`,
             { headers: { Authorization: `Bearer ${accessToken}` } }
         ).then(
             response => {
